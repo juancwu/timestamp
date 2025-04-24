da7d615574a6ee9fa975029abfc915c19a91c79f
jc <46619361+juancwu@users.noreply.github.com>
2025-04-24 18:43:20 -0400

    update auth page ui

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -14,7 +14,7 @@ export default async function AuthPage() {
 	}
 
 	return (
-		<div className="flex h-screen w-screen items-center justify-center">
+		<div className="flex w-screen items-center pt-20">
 			<div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
 				<div className="flex flex-col space-y-2 text-center">
 					<h1 className="text-2xl font-semibold tracking-tight">
