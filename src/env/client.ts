607f0ef252513c7b05a8203945154f8dc3401917
jc <46619361+juancwu@users.noreply.github.com>
2025-04-24 14:58:52 -0400

    setup t3env

--- src/env/client.ts
+++ src/env/client.ts
@@ -1,1 +1,13 @@
-export const env = {};
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+	client: {
+		// Add client-side environment variables here
+		NEXT_PUBLIC_APP_ENV: z.enum(["development", "staging", "production"]),
+	},
+	experimental__runtimeEnv: {
+		// Map client-side variables here
+		NEXT_PUBLIC_APP_ENV: process.env.NEXT_PUBLIC_APP_ENV,
+	},
+});
--- src/env/server.ts
+++ src/env/server.ts
@@ -1,1 +1,14 @@
-export const env = {};
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+	server: {
+		BETTER_AUTH_SECRET: z.string().min(1),
+		BETTER_AUTH_URL: z.string().url(),
+		TURSO_DATABASE_URL: z.string().url(),
+		TURSO_AUTH_TOKEN: z.string().min(1),
+		GITHUB_CLIENT_ID: z.string().min(1),
+		GITHUB_CLIENT_SECRET: z.string().min(1),
+	},
+	experimental__runtimeEnv: process.env,
+});
